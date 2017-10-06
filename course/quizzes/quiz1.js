92e23d6f2fb68be00cf4bb0ab0e65d6f924da14e
Alex Sun <alexs@ultra.me>
2017-10-06 16:54:05 -0700

    add quiz1.js

---
+++ course/quizzes/quiz1.js
@@ -0,0 +1,44 @@
+8; // ?
+
+
+
+'true'; // ?
+
+
+
+true; // ?
+
+
+
+!'true'; // ?
+
+
+
+undefined; // ?
+
+
+
+var x = 8;
+x; // ?
+x = x + 1;
+x; // ?
+x = 'hello world';
+x; // ?
+
+
+
+var y;
+y = undefined;
+y // ?
+
+
+
+var z = true && 2;
+var a = 5;
+(a + z); // ?
+
+
+
+/* Bonus */
+var bonus;
+!bonus // ?
