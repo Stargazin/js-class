8d9cb85aeea6bb248c09ea7eb6ae3303cfe20717
Alex Sun <alexs@ultra.me>
2017-10-10 12:28:00 -0700

    quiz 2

---
+++ course/quizzes/quiz2.js
@@ -0,0 +1,44 @@
+/* QUIZ 2
+ * Given a block of code,
+ * change `??` to produce the expected `x` value
+ * Anything in the block of code may be used,
+ * but only `??` may be replaced.
+ * `??` may be replaced with *anything* if it works.
+ * *Make it as simple as possible.*
+ */
+
+/* ------------------- */
+
+var greet = ??;
+var hello = (greet ? 8 : 2);
+var goodbye = ??;
+
+var x = (hello && goodbye);
+x; // true
+
+/* ------------------- */
+
+var apple = (?? || 19);
+var lime = (?? && 8);
+var orange = (apple - lime);
+
+x = orange;
+
+x; // 11
+
+/* ------------------- */
+
+var z = {
+	a: {
+		val: 1
+	},
+	b: 9
+};
+
+var y;
+y = ??
+
+x = y;
+x; // 8
+
+/* ------------------- */
